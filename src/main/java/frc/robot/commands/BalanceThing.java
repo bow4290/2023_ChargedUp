6ba9f4a190117b52b4897a8a57a406d3e7d67cf9
ka930 <77208047+ka930@users.noreply.github.com>
2023-01-31 18:59:57 -0500

    Hopefully this does not horribly fail and explode the robot

--- src/main/java/frc/robot/commands/BalanceThing.java
+++ src/main/java/frc/robot/commands/BalanceThing.java
@@ -10,22 +10,26 @@ import edu.wpi.first.wpilibj2.command.PIDCommand;
 
 public class BalanceThing extends PIDCommand {
     public BalanceThing(Swerve swerve) {
-        super(
-            new PIDController(1, 0, 0),
-            // Based on pitch
-            swerve::getTiltMagnitude,
-            // We want to gain a pitch of 0
-            0,
-            // Output moves bot
-            output -> {
+      super(
+          new PIDController(1, 0, 0),
+          // Based on pitch
+          swerve::getTiltMagnitude,
+          // We want to gain a pitch of 0
+          0,
+          // Output moves bot
+          output -> {
+            output = Math.abs(output);
+            if (output > 0.05) {
               Rotation2d rot = swerve.getTiltDirection();
-              output = Math.abs(output);
               swerve.drive(
                 new Translation2d(output * rot.getCos(), output * rot.getSin()), 0, false, true);
-            },
-            swerve
-        );
-  
+            } else {
+              swerve.lockChargeStation();
+            }
+          },
+          swerve
+      );
+
       // Set the controller tolerance - the delta tolerance ensures the robot is stationary at the
       // setpoint before it is considered as having reached the reference
       getController()
@@ -33,11 +37,19 @@ public class BalanceThing extends PIDCommand {
       // Account for wpilib bug?
       getController().setSetpoint(0);
     }
-  
+  /* 
     @Override
     public boolean isFinished() {
       // End when the controller is at the reference.
       return getController().atSetpoint();
     }
+
+    @Override
+    public void end(boolean insuccessful) {
+      super.end(insuccessful);
+      if (!insuccessful) 
+        swerve.lockChargeStation();
+    }
+    */
   }
   
