4fece3afc1c3951e786dce086fba1234fa59f2d5
ka930 <77208047+ka930@users.noreply.github.com>
2023-03-07 21:32:49 -0500

    add teleopswerve.execute back

--- src/main/java/frc/robot/commands/swerve/TeleopSwerve.java
+++ src/main/java/frc/robot/commands/swerve/TeleopSwerve.java
@@ -1,6 +1,9 @@
 package frc.robot.commands.swerve;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Constants;
 import frc.robot.subsystems.Swerve;
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
@@ -28,5 +31,19 @@ public class TeleopSwerve extends CommandBase {
   }
 
   @Override
-  public void execute() {}
+  public void execute() {
+    /* Get Values, Deadband*/
+    double forwardBackwardVal =
+        MathUtil.applyDeadband(forwardBackwardSup.getAsDouble(), Constants.stickDeadband);
+    double leftRightVal =
+        MathUtil.applyDeadband(leftRightSup.getAsDouble(), Constants.stickDeadband);
+    double rotationVal = MathUtil.applyDeadband(rotationSup.getAsDouble(), Constants.stickDeadband);
+
+    /* Drive */
+    s_Swerve.drive(
+        new Translation2d(forwardBackwardVal, leftRightVal).times(Constants.Swerve.maxSpeed),
+        rotationVal * Constants.Swerve.maxAngularVelocity,
+        !robotCentricSup.getAsBoolean(),
+        true);
+  }
 }
