a23ace48d72655f1c78e914c9602337b3b318128
4290Laptop <knightdaleknights44@gmail.com>
2023-04-01 10:21:37 -0400

    commit

---
+++ src/main/java/frc/robot/subsystems/LED.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LED extends SubsystemBase {
+
+  public LED() {}
+
+  /* *
+  @Override
+  public void periodic() {
+    SmartDashboard.putNumber("Intake Status: ", lastPowerSet);
+    double leftCurrent = leftIntake.getOutputCurrent();
+    SmartDash
+  }*/
+}
