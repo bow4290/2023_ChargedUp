410ff239c19528e1389cf730e6ed0534d2ae6bd5
ka930 <77208047+ka930@users.noreply.github.com>
2023-05-27 14:06:16 -0400

    Sorry I just remembered to push changes from tuesday lol

--- src/main/java/frc/robot/subsystems/LED.kt
+++ src/main/java/frc/robot/subsystems/LED.kt
@@ -2,9 +2,11 @@ package frc.robot.subsystems
 
 import edu.wpi.first.wpilibj.AddressableLED
 import edu.wpi.first.wpilibj.AddressableLEDBuffer
+import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj.util.Color
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import kotlin.math.floor
 
 private const val LED_LENGTH = 50
 private const val MAX_CURRENT = 1500.0 // See VRM datasheet
@@ -17,7 +19,8 @@ class LED : SubsystemBase() {
 
     init {
         led.setLength(LED_LENGTH)
-        solid(Color.kGreen)
+        //solid(Color.kGreen)
+        rainbow2().schedule()
         show()
         led.start()
     }
@@ -47,7 +50,7 @@ class LED : SubsystemBase() {
                 val color = buffer.getLED(i)
                 buffer.setLED(i, Color(color.red / div, color.green / div, color.blue / div))
             }
-            println("Warning: LED strip exceeded limit by $div")
+            //println("Warning: LED strip exceeded limit by $div")
         }
     }
 
@@ -65,5 +68,74 @@ class LED : SubsystemBase() {
             solid(c)
             show()
         }
+    }
+
+    fun rainbow(): Command {
+        val period = 5;
+        return run {
+            var time = (Timer.getFPGATimestamp() % period) * (180 / period)
+            var col = Color.fromHSV(time.toInt(), 255, 255);
+            solid(col)
+            show()
+        }.ignoringDisable(true)
+    }
+
+    fun rainbow2(): Command {
+        val period = 1;
+        val increment = 10;
+        return run {
+            var time = (Timer.getFPGATimestamp() % period) * (180 / period)
+            for (i in 0 until LED_LENGTH) {
+                var col = Color.fromHSV(time.toInt() + increment * i, 255, 255);
+                setPixel(i, col)
+            }
+            show()
+        }.ignoringDisable(true)
+    }
+
+    fun sparkles(): Command {
+        val period = 0.4
+        return run {
+            //var time = floor((Timer.getFPGATimestamp() % period) * (10 / period))
+            for (i in 0 until LED_LENGTH) {
+                if (Math.random() < 0.02) {
+                    setPixel(i, Color.kWhite)
+                } else {
+                    setPixel(i, Color.kBlack)
+                }
+            }
+            show()
+        }.ignoringDisable(true)
+    }
+
+    fun snow(): Command {
+        val period = 1;
+        val increment = 10;
+        return run {
+            var time = (Timer.getFPGATimestamp() % period) * (255 / period)
+            for (i in 0 until LED_LENGTH) {
+                var col = Color.fromHSV(200, 255, time.toInt() + increment * i);
+                setPixel(i, col)
+            }
+            show()
+        }.ignoringDisable(true)
+    }
+
+    fun trans(): Command {
+        //val period = 1;
+        //val increment = 10;
+        return run {
+            //var time = (Timer.getFPGATimestamp() % period) * (180 / period)
+            var blue = Color(91/255, 206/255, 250/255)
+            var pink = Color(245/255, 169/255, 184/255)
+            var white = Color(255/255, 255/255, 255/255)
+
+            var colors = arrayListOf(blue, pink, white, pink, blue);
+
+            for (i in 0 until LED_LENGTH) {
+                setPixel(i, colors[floor(((i / 10).toDouble())).toInt()])
+            }
+            show()
+        }.ignoringDisable(true)
     }
 }
